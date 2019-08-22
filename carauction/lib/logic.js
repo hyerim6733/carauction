8f9006f1f21b18ed30d5891e07273bf078807295
hyerim <shrim657@gmail.com>
2019-08-22 12:28:17 +0900

    initializing car hyerim_22

--- carauction/lib/logic.js
+++ carauction/lib/logic.js
@@ -18,26 +18,95 @@
  */
 
 /**
- * Sample transaction
- * @param {com.betweak.carauction.SampleTransaction} sampleTransaction
+ * Close the bidding for a vehicle listing and choose the
+ * highest bid that is over the asking price
+ * @param {com.betweak.carauction.CloseBidding} closeBidding - the closeBidding transaction
  * @transaction
  */
-async function sampleTransaction(tx) {
-    // Save the old value of the asset.
-    const oldValue = tx.asset.value;
+async function closeBidding(closeBidding) {
+    const listing = closeBidding.listing;
+    if (listing.state !== "FOR_SALE") {
+        throw new Error("Listing is not FOR SALE");
+    }
 
-    // Update the asset with the new value.
-    tx.asset.value = tx.newValue;
+    // by default we mark the listing as RESERVE_NOT_MET
+    listing.state = "RESERVE_NOT_MET"; // 요청 불가
+    let highestOffer = null; // 최고가
+    let buyer = null; // 구매자
+    let seller = null; // 판매자
 
-    // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('com.betweak.carauction.SampleAsset');
-    // Update the asset in the asset registry.
-    await assetRegistry.update(tx.asset);
+    if(listing.offers && listing.offers.length > 0 ) {
+        listing.offers.sort(function(a, b){
+            return b.bidPrice - a.bidPrice;
+        }) ;
 
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('com.betweak.carauction', 'SampleEvent');
-    event.asset = tx.asset;
-    event.oldValue = oldValue;
-    event.newValue = tx.newValue;
-    emit(event);
+        // 최고가
+        highestOffer = listing.offers[0];
+        if(highestOffer.bidPrice >= listing.reservePrice) {
+            // 최고가가 예약금 이상이면
+            // mark the listing as SOLD
+            listing.state = "SOLD";
+            buyer = highestOffer.member;
+            seller = listing.car.owner;
+
+            // 판매자 잔고 업데이트
+            console.log("#### 거래전 판매자 잔고 : " + seller.balance);
+            seller.balance += highestOffer.bidPrice;
+            console.log("#### 거래후 판매자 잔고 : " + seller.balance );
+            // 구매자 잔고 업데이트
+            console.log("#### 거래전 구매자 잔고 : " + buyer.balance);
+            buyer.balance -= highestOffer.bidPrice;
+            console.log("#### 거래후 구매자 잔고 : " + buyer.balance );
+            // 차의 주인 변경
+            listing.car.owner = buyer;
+            // 옥션 초기화
+            listing.offers = null;
+
+        }
+    }
+
+    // 최고가 존재시
+    if(highestOffer) {
+        // 자동차 정보 변경(주인 변경)
+        const carRegistry = await getassetRegistry("com.betweak.carauction.Car");
+        await carRegistry.update(listing.car);
+    }
+
+    // 자동차 경매 정보 변경
+    const carListingRegistry = await getAssetRegistry("com.betweak.carauction.carListing");
+    await carListingRegistry.update(listing);
+
+    // 자동차 경매 정보가 SOLD이면
+    if(listing.state === "SOLD") {
+        // 구매자 정보 변경
+        const userRegistry = await getParticipantRegistry("com.betweak.carauction.Member");
+
+        // User 정보 업데이트 
+        await userRegistry.updateAll([buyer, seller]);
+    }
+}
+
+/** 
+* Make on Offer for a carListing 
+* @param {com.betweak.carauction.Offer} offer - the opper
+* @transaction
+*/
+async function makeOffer(offer) {
+    let listing = offer.listing;
+    if (listing.state !== "FOR_SALE") {
+        // 팔리는 중이 아니라면 에러
+        throw new Error("Listing is not FOR SLAE");
+    }  
+    if(!listing.offers) {
+        // 최초 입찰이면 빈 배열 만듦
+        listing.offers = [];
+    }
+    listing.offers.push(offer);
+
+    // 리스팅 업데이트
+    const carListingRegistry = await getAssetRegistry (
+        "com.betweak.carauction.CarListing"
+    );
+    await carListingRegistry.update(listing);
+
 }
